8882cd7ef7e79d802a792029c3e8a145148b18f5
Brandon Lim <brandonlimchi@gmail.com>
2021-12-07 08:37:45 -0800

    style cursor appropriately when hovering various elements

--- app/javascript/components/Ticket.jsx
+++ app/javascript/components/Ticket.jsx
@@ -8,7 +8,7 @@ const Ticket = (props) => {
 
   return (
     <div className="ticket column-item">
-      <p>{ props.desc }</p>
+      <p className="ticket-description">{ props.desc }</p>
       <div className="ticket-destroy" onClick={handleTicketDestroy}>X</div>
     </div>
   );
