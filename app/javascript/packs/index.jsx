b89b9cc37703f38135f7c03a4d169441401a8628
Brandon Lim <brandonlimchi@gmail.com>
2021-12-07 03:48:12 -0800

    move styles to their own respective css files

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from '../components/Dashboard';
-import '../../assets/stylesheets/reset.css';
-import '../../assets/stylesheets/dragula.min.css';
-import '../../assets/stylesheets/style.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
