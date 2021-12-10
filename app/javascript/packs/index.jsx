22707a415d5da2cff7eab470da82455026d31e18
Brandon Lim <brandonlimchi@gmail.com>
2021-12-10 11:59:06 -0800

    set csrf token for axios headers

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from '../components/Dashboard';
+import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let token = document.head.querySelector('meta[name="csrf-token"]');
+  if(token) axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+
   ReactDOM.render(
     <React.StrictMode>
       <Dashboard />
