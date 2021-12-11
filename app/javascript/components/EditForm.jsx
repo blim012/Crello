9c7f726e77e376916331ae05d5b98a0ed3cb1680
Brandon Lim <brandonlimchi@gmail.com>
2021-12-11 00:26:49 -0800

    implement EditForm component

---
+++ app/javascript/components/EditForm.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+
+const EditForm = (props) => {
+  const {title, handleSubmit} = props;
+  const [formOpen, setFormOpen] = useState(false);
+  const [textValue, setTextValue] = useState(title);
+
+  const toggleForm = () => {
+    setTextValue(title);
+    setFormOpen(!formOpen);
+  };
+
+  const handleChange = (e) => {
+    setTextValue(e.target.value);
+  };
+
+  const highlightInput = (e) => {
+    let input = e.currentTarget;
+    input.select();
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(textValue);
+    toggleForm();
+  };
+
+  return (
+    <>
+      {formOpen
+        ?
+        <form className="edit-form prevent-drag-scroll" onSubmit={onSubmit}>
+          <input
+            type="text"
+            value={textValue} 
+            onChange={handleChange}
+            autoFocus={true}
+            onFocus={highlightInput}
+            required />
+          <div className="edit-form-buttons">
+            <input type="submit" className="edit-form-submit" value={'Edit'} />
+            <button className="edit-form-cancel" onClick={toggleForm}>Cancel</button>
+          </div>
+        </form>
+
+        :
+        <div className="edit-form-text prevent-drag-scroll" onClick={toggleForm}>{title}</div>
+      }
+    </>
+  );
+};
+
+export default EditForm;
