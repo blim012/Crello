599e0149cf50ff84cc3f730fd019e64300054664
Brandon Lim <brandonlimchi@gmail.com>
2021-12-12 06:53:14 -0800

    remove admin column from BoardUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_143414) do
+ActiveRecord::Schema.define(version: 2021_12_12_145037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_12_12_143414) do
   create_table "board_users", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "board_id"
-    t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["board_id"], name: "index_board_users_on_board_id"
