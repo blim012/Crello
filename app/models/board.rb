fe3800ea59d70de08f4a1c006bc2b0ed4ee1d1c6
Brandon Lim <brandonlimchi@gmail.com>
2021-12-13 12:08:20 -0800

    add board model validations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,8 @@
 class Board < ApplicationRecord
-  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id', required: true
   has_many :board_users, dependent: :destroy
   has_many :invited_users, through: :board_users, source: :user
   has_many :columns, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 80 }
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Board, type: :model do
   describe 'associations' do
-    it { should belong_to(:creator).class_name('user').required }
+    it { should belong_to(:creator).class_name('User').required }
     it { should have_many(:board_users) }
-    it { should have_many(:invited_users).class_name('user') }
+    it { should have_many(:invited_users).class_name('User') }
     it { should have_many(:columns) }
   end
 
